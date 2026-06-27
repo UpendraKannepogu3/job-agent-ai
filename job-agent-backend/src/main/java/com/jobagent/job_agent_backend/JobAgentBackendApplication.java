3cae2a709a98620c5202efa4e5aa184804c7ebf7
Upendra Kannepogu <upendra.kannepogu3b5@gmail.com>
2026-06-27 19:53:40 +0530

    chore: add spring boot project and application.yml

---
+++ job-agent-backend/src/main/java/com/jobagent/job_agent_backend/JobAgentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jobagent.job_agent_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAgentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAgentBackendApplication.class, args);
+	}
+
+}
---
+++ job-agent-backend/src/test/java/com/jobagent/job_agent_backend/JobAgentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobagent.job_agent_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAgentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
